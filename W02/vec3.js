5701935588b61537e51d5d88cf2602e831518890
Emi-Ko@note <koyama.study@google.com>
2020-05-13 16:19:32 +0900

    update for Task2

--- W02/vec3.js
+++ W02/vec3.js
@@ -25,4 +25,61 @@ class Vec3
     return this.x + this.y + this.z;
   }
 
+
+  // Task2
+
+  // Min method
+  min ()
+  {
+    let min;
+    if(this.x < this.y){
+      min = this.x;
+    }
+    else{
+      min = this.y;
+    }
+    if(this.z < min){
+      min = this.z;
+    }
+    return min;
+  }
+
+  // Mid method
+  mid ()
+  {
+    let min = this.min();
+    let max = this.max();
+    if(this.x != min){
+      if(this.x != max){
+        return this.x;
+      }
+    }
+    if(this.y != min){
+      if(this.y != max){
+        return this.y;
+      }
+    }
+    if(this.z != min){
+      if(this.z != max){
+        return this.z;
+      }
+    }
+  }
+
+  // Max method
+  max ()
+  {
+    let max;
+    if( this.x < this.y ){
+      max = this.y;
+    }
+    else{
+      max = this.x;
+    }
+    if ( max < this.z ){
+      max = this.z;
+    }
+    return max;
+  }
+
 }
